69919abe76ffa2a987cc055f73c0c9e3cb3546ea
hakim <kimleonafricain@gmail.com>
2020-05-15 00:39:49 +0200

    *

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan')
 
 const app = new express();
+// middelwares 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(morgan("dev"))
+
+app.get('/', (req, res) => {
+    res.send('hello there')
+})
 
 app.listen(3000, ()=> console.log(`server listening on port 3000`))
